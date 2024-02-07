0dcab924a428fc7ea966d707cf9d6b1a114d919e
jtmer276 <jtmerrill276@gmail.com>
2024-02-07 14:46:00 -0500

    lots
    wdwdwd

---
+++ page9.java
@@ -0,0 +1,14 @@
+/*************
+ *Project: Page Nine 
+ *Programmer: Jules Merrill
+ *Date: February 7, 2006
+ *Program Name: page9.java
+ **************/
+public class page9
+{
+        public static void main (String[] args)
+        {
+            System.out.println("Hi"); //message to user
+            System.out.println("LabOneA"); //assignment
+        }
+}
